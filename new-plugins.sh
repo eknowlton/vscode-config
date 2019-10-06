722728c941c295d08e135c8749dc96168e4dd272
Ethan Knowlton <eknowlton@gmail.com>
2019-10-06 17:56:56 -0400

    Create new-plugins.sh

---
+++ new-plugins.sh
@@ -0,0 +1,6 @@
+code --install-extension belfz.search-crates-io
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bungcip.better-toml
+code --install-extension rust-lang.rust
+code --install-extension serayuzgur.crates
+code --install-extension vscodevim.vim
